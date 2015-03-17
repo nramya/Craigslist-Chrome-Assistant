110aabdaa23f24619b25da39e1a1c9c75f4a4e62
Ramya Narayanaswamy <ramya.narayanaswamy@gmail.com>
2015-03-17 13:39:39 -0700

    added mailto

--- js/main.js
+++ js/main.js
@@ -1,36 +1,45 @@
-//function createElement(type, parent, innerTML, className) {
-//    var element = document.createElement(type);
-//    if (parent) parent.appendChild(element);
-//    if (innerHTML) element.innerHTML = innerHTML;
-//    if (className) element.className = className;
-//    return element;
-//}
-
-
 var btnReply = document.querySelector('.reply_button');
 var btnSchedule = document.createElement('button');
 btnSchedule.textContent = 'Schedule';
 btnSchedule.classList.add('inserted_button');
 btnReply.parentNode.insertBefore(btnSchedule, btnReply.nextSibling);
+btnReply.click();
+var returnemail = document.querySelector('.returnemail');
+btnReply.click();
+var emailObj = {};
+//var contact_info = returnemail.querySelector('.reply_options').querySelectorAll('ul');
 
 btnSchedule.addEventListener('click', function (event) {
-    console.log('clicked!');
-    extractEmail();
-    extractPhone();
-    prepareEmailDraft();
+    emailObj = extractEmailFromPost();
+    extractPhoneFromPost();
+    console.log(emailObj);
+    if(emailObj.emailid) prepareEmailDraft();
     showCalendar();
 });
 
-function extractEmail () {
-
+function extractEmailFromPost () {
+    return {
+        link: document.querySelector('#replylink').href,
+        postid: document.querySelector('#replylink').href.split('/').pop(),
+        subject: returnemail.querySelector('title').innerHTML,
+        emailid: (returnemail.querySelector('.anonemail')) ? returnemail.querySelector('.anonemail').innerHTML : null
+    }
 }
 
-function extractPhone () {
-
+function extractPhoneFromPost () {
+    emailObj.phone = returnemail.querySelector('.reply_options').querySelectorAll('ul')[2].querySelector('li').innerHTML;
 }
 
 function prepareEmailDraft () {
+    var mailtoString = 'mailto:'+ encodeURIComponent(emailObj.emailid)
+        + '?subject=' + encodeURIComponent(emailObj.subject) + '&body=' + encodeURIComponent(emailObj.link);
+    console.log(mailtoString);
 
+    returnemail.querySelector('.reply_options').querySelectorAll('ul')[3]
+        .querySelector('li').querySelector('a').href = mailtoString;
+
+    console.log(returnemail.querySelector('.reply_options').querySelectorAll('ul')[3]
+        .querySelector('li').querySelector('a').href);
 }
 
 function showCalendar () {
