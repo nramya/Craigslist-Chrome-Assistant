4824c531d3d52c319261a9cc605ac535f3bdf75d
Ramya Narayanaswamy <ramya.narayanaswamy@gmail.com>
2015-03-18 14:45:02 -0700

    added button for email template

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
 var btnReply = document.querySelector('.reply_button');
-var btnSchedule = document.createElement('button');
-btnSchedule.textContent = 'Schedule';
-btnSchedule.classList.add('inserted');
-var gmail = document.createElement('a');
-gmail.innerHTML = 'Gmail';
-gmail.classList.add('inserted');
-btnReply.parentNode.insertBefore(btnSchedule, btnReply.nextSibling);
-btnReply.click();
-var returnemail = document.querySelector('.returnemail');
-btnReply.click();
-var emailObj = {};
-//var contact_info = returnemail.querySelector('.reply_options').querySelectorAll('ul');
 
-//emailObj = extractEmailFromPost();
-//gmail.href = emailObj;
+var btnSchedule = document.createElement('button');
+btnSchedule.textContent = 'Send Email';
+btnSchedule.classList.add('inserted');
+
+var btnTemplate = document.createElement('button');
+btnTemplate.textContent = 'Email Template';
+btnTemplate.classList.add('inserted');
+
+btnReply.parentNode.insertBefore(btnSchedule, btnReply.nextSibling);
+btnReply.parentNode.insertBefore(btnTemplate, btnReply.nextSibling);
+
+btnReply.click(); // To enable the drop down menu -- to read contact information
+var returnemail = document.querySelector('.returnemail');
+btnReply.click(); // To close the menu to hide the effect of simulated click
+
+var emailObj = {};
 
 btnSchedule.addEventListener('click', function (event) {
     emailObj = extractEmailFromPost();
     extractPhoneFromPost();
-    //console.log(emailObj);
     if(emailObj.emailid) prepareEmailDraft();
     showCalendar();
+});
+
+btnTemplate.addEventListener('click', function (event) {
+    window.open('email_dialog.html');
 });
 
 function extractEmailFromPost () {
@@ -39,13 +44,9 @@ function extractPhoneFromPost () {
 function prepareEmailDraft () {
     var mailtoString = 'mailto:'+ encodeURIComponent(emailObj.emailid)
         + '?subject=' + encodeURIComponent(emailObj.subject) + '&body=' + encodeURIComponent(emailObj.link);
-    console.log(mailtoString);
 
     returnemail.querySelector('.reply_options').querySelectorAll('ul')[3]
         .querySelector('li').querySelector('a').href = mailtoString;
-
-    console.log(returnemail.querySelector('.reply_options').querySelectorAll('ul')[3]
-        .querySelector('li').querySelector('a').href);
 
     sendEmail(mailtoString);
 }
